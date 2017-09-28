55874ce41ab1c5d4e921302a1e3f9ae880f75fa2
Petar Dakov <p.dakov97@gmail.com>
2017-09-28 17:55:54 +0300

    Created Project to host solutions of Conditional Statements and Loops Exercises

---
+++ PFE.CondStmtsNLoops/Exercise01/ChooseADrink.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace SoftUniJudgeSolutions
+{
+    internal class ChooseADrink
+    {
+        private static String profession;
+
+        /// <summary>
+        /// Collect output here instead of directly writing to Console.
+        /// This noticeably improves the execution times.
+        /// </summary>
+        private static StringBuilder result = new StringBuilder();
+
+        /// <summary>
+        /// Simple 3-steps approach to any exercise
+        /// </summary>
+        private static void Main(string[] args)
+        {
+            ReadInput();
+            Solve();
+            PrintResult();
+        }
+
+        /// <summary>
+        /// Prints the collected output when solving has completed.
+        /// </summary>
+        private static void PrintResult()
+        {
+            Console.WriteLine(result.ToString());
+        }
+
+        /// <summary>
+        /// Read console input here.
+        /// Assign to static variables,
+        /// in order do be accessible across the other static methods
+        /// </summary>
+        private static void ReadInput()
+        {
+            profession = Console.ReadLine();
+        }
+
+        private static void Solve()
+        {
+            switch (profession)
+            {
+                case "Athlete":
+                    result.Append("Water");
+                    break;
+
+                case "Businessman":
+                case "Businesswoman":
+                    result.Append("Coffee");
+                    break;
+
+                case "Softuni Student":
+                    result.Append("Beer");
+                    break;
+
+                default:
+                    result.Append("Tea");
+                    break;
+            }
+        }
+    }
+}
